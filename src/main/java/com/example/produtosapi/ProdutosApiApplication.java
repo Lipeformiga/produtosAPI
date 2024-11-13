e9bda8267dae5fe7a3658732ee315cb41573f72b
Filipe Formigari <formigari2804@gmail.com>
2024-11-13 16:09:33 -0300

    a

--- src/main/java/com/example/produtosapi/ProdutosApiApplication.java
+++ src/main/java/com/example/produtosapi/ProdutosApiApplication.java
@@ -4,7 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@RestController
 public class ProdutosApiApplication {
+
+//	@GetMapping("/hello-world")
+//	public String helloWorld(){
+//		return "Hello World";
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProdutosApiApplication.class, args);
