30194d8a43396c10a5169f041697891ff91e6a91
Filipe Formigari <formigari2804@gmail.com>
2024-11-27 14:08:18 -0300

    15

--- src/main/java/com/example/produtosapi/ProdutoRepository/repository.java
+++
@@ -1,9 +0,0 @@
-package com.example.produtosapi.ProdutoRepository;
-
-import com.example.produtosapi.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface repository extends JpaRepository<Produto, String> {
-
-    
-}
